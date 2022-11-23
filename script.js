195bbd818d5d75ceb9dc96d0104535c3775f93f3
Elena Ristea <Elena.Ristea@nology.io>
2022-11-23 16:28:20 +0000

    added function

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ import { pokemonArray } from "./data/pokemon.js";
 const cardContainer = document.querySelector(".card-container");
 
 console.log(pokemonArray);
+
+// in the paragraph "name" + " " + "#id" + " " + "is a" + " " + "types[0]" + " " + "&" + " " + "types[1]" + " " + "type pokemon."
+
+const renderPokemon = (pokemonArray) => {
+  let htmlString = "";
+
+  pokemonArray.forEach((pokemon) => {
+    htmlString += (
+      <section>
+        <h2>${pokemonArray.name}</h2>
+        <p></p>
+      </section>
+    );
+  });
+};
